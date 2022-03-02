6c576067ef3e1566c90465d966f7451ce441ec10
Miguel Carvalho <miguelc@tuta.io>
2022-03-02 14:44:01 +0000

    Scaffolding for components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>App</div>
+    </div>
+  );
 };
 
 export default App;
