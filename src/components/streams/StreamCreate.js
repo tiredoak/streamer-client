9e950a51ef9720c1c0bff7424313d376475b3876
Miguel Carvalho <miguelc@tuta.io>
2022-03-03 15:30:32 +0000

    Add validate function

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -32,6 +32,18 @@ class StreamCreate extends React.Component {
   }
 }
 
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = 'You must enter a title';
+  }
+  if (!formValues.description) {
+    errors.title = 'You must enter a description';
+  }
+
+  return errors;
+};
+
 export default reduxForm({
   form: 'streamCreate',
 })(StreamCreate);
