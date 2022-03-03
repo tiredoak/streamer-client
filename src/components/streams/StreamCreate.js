200b7f5c1c2d38e128d36bde1ea0917cb65a5685
Miguel Carvalho <miguelc@tuta.io>
2022-03-03 14:57:35 +0000

    Redux From is connected to the StreamCreate component

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
+  renderInput(formProps) {
+    console.log(formProps);
+    return (
+      <input
+        onChange={formProps.input.onChange}
+        value={formProps.input.value}
+      />
+    );
+  }
+
   render() {
-    return <div>StreamCreate</div>;
+    return (
+      <form>
+        <Field name='title' component={this.renderInput} />
+        <Field name='description' component={this.renderInput} />
+      </form>
+    );
   }
 }
 
-export default StreamCreate;
+export default reduxForm({
+  form: 'streamCreate',
+})(StreamCreate);
