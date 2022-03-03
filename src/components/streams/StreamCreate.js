d5f4b538494a70582c39d61d983dc4970ecbef42
Miguel Carvalho <miguelc@tuta.io>
2022-03-03 14:27:05 +0000

    Refactored StreamCreate to class-based component

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+  render() {
+    return <div>StreamCreate</div>;
+  }
+}
 
 export default StreamCreate;
