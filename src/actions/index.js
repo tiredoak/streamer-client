aeee0e257fcffa0d4cfa3864d305e8f03b7e8208
Miguel Carvalho <miguelc@tuta.io>
2022-03-04 16:59:57 +0000

    Finished StreamEdit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import StreamShow from '../components/streams/StreamShow';
 import {
   SIGN_IN,
@@ -27,7 +28,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
   const { userId } = getState().auth;
   const response = await streams.post('/streams', { ...formValues, userId });
   dispatch({ type: CREATE_STREAM, payload: response.data });
-  // programmatic navigation to get user back to root route
+  history.push('/');
 };
 
 export const fetchStreams = () => async (dispatch) => {
--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -12,7 +12,9 @@ class StreamList extends React.Component {
     if (stream.userId === this.props.currentUserId) {
       return (
         <div className='right floated content'>
-          <button className='ui button primary'>EDIT</button>
+          <Link to={`/streams/edit/${stream.id}`} className='ui button primary'>
+            EDIT
+          </Link>
           <button className='ui button negative'>DELETE</button>
         </div>
       );
