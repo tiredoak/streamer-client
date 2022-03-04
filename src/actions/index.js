7cbb0cb904ef550b7e6fc7e21028cbffacbc1f84
Miguel Carvalho <miguelc@tuta.io>
2022-03-04 15:40:00 +0000

    Created own history object

--- src/actions/index.js
+++ src/actions/index.js
@@ -27,6 +27,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
   const { userId } = getState().auth;
   const response = await streams.post('/streams', { ...formValues, userId });
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  // programmatic navigation to get user back to root route
 };
 
 export const fetchStreams = () => async (dispatch) => {
