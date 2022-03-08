af132a19d2596e6d6a72fe76856201d69456cf1d
Miguel Carvalho <miguelc@tuta.io>
2022-03-08 09:57:52 +0000

    Add routes for StreamShow

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -32,7 +32,9 @@ class StreamList extends React.Component {
           {this.renderAdmin(stream)}
           <i className='large middle aligned icon camera' />
           <div className='content'>
-            {stream.title}
+            <Link className='header' to={`/streams/${stream.id}`}>
+              {stream.title}
+            </Link>
             <div className='description'>{stream.description}</div>
           </div>
         </div>
