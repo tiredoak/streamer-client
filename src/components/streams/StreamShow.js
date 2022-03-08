86468aa029d95d6a0543e0d9854727e8aa47864c
Miguel Carvalho <miguelc@tuta.io>
2022-03-08 12:13:29 +0000

    Clean up resources

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -20,6 +20,10 @@ class StreamShow extends React.Component {
     this.buildPlayer();
   }
 
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
   buildPlayer() {
     if (this.player || !this.props.stream) {
       return;
